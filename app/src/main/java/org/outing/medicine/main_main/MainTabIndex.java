650931e173c0d447eb6163f0fa7425f1dc425279
LinFeng <china2012syl@163.com>
2015-11-04 19:34:57 +0800

    添加了退出登录。

--- app/src/main/java/org/outing/medicine/main_main/MainTabIndex.java
+++ app/src/main/java/org/outing/medicine/main_main/MainTabIndex.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 
 import org.outing.medicine.R;
@@ -17,8 +16,12 @@ import org.outing.medicine.fun_remind.RemindMain;
 import org.outing.medicine.fun_tools.ToolsMain;
 import org.outing.medicine.illness_manage.IllnessManageActivity;
 import org.outing.medicine.personal_center.PersonalCenterActivity;
+import org.outing.medicine.start.Login;
+import org.outing.medicine.start.UserTool;
+import org.outing.medicine.tools.connect.ConnectTool;
 
 public class MainTabIndex extends Fragment implements OnClickListener {
+    public static final int LOG_OUT = 1;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -45,26 +48,40 @@ public class MainTabIndex extends Fragment implements OnClickListener {
         switch (v.getId()) {
             case R.id.index_tools:
                 intent = new Intent(getActivity(), ToolsMain.class);
+                startActivity(intent);
                 break;
             case R.id.index_remind:
                 intent = new Intent(getActivity(), RemindMain.class);
+                startActivity(intent);
                 break;
             case R.id.index_illness:
                 intent = new Intent(getActivity(), IllnessManageActivity.class);
+                startActivity(intent);
                 break;
             case R.id.index_query:
                 intent = new Intent(getActivity(), DrugMain.class);
+                startActivity(intent);
                 break;
             case R.id.index_knowledge:
                 intent = new Intent(getActivity(), KnowMain.class);
+                startActivity(intent);
                 break;
             case R.id.index_my:
                 intent = new Intent(getActivity(), PersonalCenterActivity.class);
+                startActivityForResult(intent, 0);
                 break;
-        }
-        if (intent != null) {
-            startActivity(intent);
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == LOG_OUT) {
+            UserTool.saveUser(getActivity(), "", "");
+            ConnectTool.saveCookie(getActivity(), "");
+            Intent intent = new Intent(getActivity(), Login.class);
+            startActivity(intent);
+            getActivity().finish();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
--- app/src/main/java/org/outing/medicine/personal_center/PersonalCenterActivity.java
+++ app/src/main/java/org/outing/medicine/personal_center/PersonalCenterActivity.java
@@ -11,11 +11,8 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ToggleButton;
 
-
-import com.baidu.location.BDNotifyListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
-
 
 import org.json.JSONObject;
 import org.outing.medicine.LocationApplication;
@@ -25,6 +22,7 @@ import org.outing.medicine.contact.ContactTool;
 import org.outing.medicine.fun_tools.WidgetImage;
 import org.outing.medicine.fun_tools.WidgetShow;
 import org.outing.medicine.fun_tools.WidgetTool;
+import org.outing.medicine.main_main.MainTabIndex;
 import org.outing.medicine.tools.TActivity;
 import org.outing.medicine.tools.connect.Connect;
 import org.outing.medicine.tools.connect.ConnectDialog;
@@ -38,11 +36,11 @@ import org.outing.medicine.tools.connect.ServerURL;
  */
 public class PersonalCenterActivity extends TActivity {
     private LocationClient mLocationClient;
-    private ToggleButton locationButton=null,contactButton=null;
-    private Button centerBtn,outBtn;
-    private EditText editName,editSex,editAge,
-            editIll,editLocation, editContact,editradius;
-    private String name,age,sex,ill,location,contact,radius;
+    private ToggleButton locationButton = null, contactButton = null;
+    private Button centerBtn, outBtn;
+    private EditText editName, editSex, editAge,
+            editIll, editLocation, editContact, editradius;
+    private String name, age, sex, ill, location, contact, radius;
     private String provider;
     private double latitude;
     private double longitude;
@@ -54,24 +52,24 @@ public class PersonalCenterActivity extends TActivity {
     public void onCreate() {
         setContentView(R.layout.activity_person_center);
         setTitle("我的设置");
-        locationApplication= (LocationApplication) getApplication();
+        locationApplication = (LocationApplication) getApplication();
         setTitleBackColor(R.color.btn_6_normal);
-		showBackButton();
+        showBackButton();
         showMenuButton();
-        try{
+        try {
             showPersonalInfo();
-        }catch (Exception e){
+        } catch (Exception e) {
             showToast("加载个人信息异常，请联网使用");
         }
         //设置完成的图片
         ((ImageButton) findViewById(R.id.top_menu))
                 .setBackgroundResource(R.drawable.yes);
         init();
-        mLocationClient = ((LocationApplication)getApplication()).mLocationClient;
-        SharedPreferences pref=getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE);
+        mLocationClient = ((LocationApplication) getApplication()).mLocationClient;
+        SharedPreferences pref = getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE);
         //设置两个按钮初始值
-        locationButton.setChecked( pref.getBoolean("locationButton",false));
-        contactButton.setChecked(pref.getBoolean("contactButton",false));
+        locationButton.setChecked(pref.getBoolean("locationButton", false));
+        contactButton.setChecked(pref.getBoolean("contactButton", false));
         //设置地理信息
         locationButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -80,21 +78,21 @@ public class PersonalCenterActivity extends TActivity {
                 Boolean weatherOn = isChecked;
                 if (weatherOn) {
 
-                    try{
+                    try {
                         initLocation();
                         mLocationClient.start();//定位SDK start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
-                        SharedPreferences.Editor editor=getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
+                        SharedPreferences.Editor editor = getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
                         editor.putBoolean("locationButton", true);
                         editor.commit();
 
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         showToast("定位功能请联网使用");
                     }
 
 
-                }else {
+                } else {
                     mLocationClient.stop();
-                    SharedPreferences.Editor editor=getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
+                    SharedPreferences.Editor editor = getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
                     editor.putBoolean("locationButton", false);
                     editor.commit();
                 }
@@ -107,20 +105,20 @@ public class PersonalCenterActivity extends TActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Log.d("test", "isChecked" + isChecked);
                 Boolean weatherOn = isChecked;
-                AnContact contact = ContactTool.getAnContact(PersonalCenterActivity.this,0 );
-                Log.d("test", "contact" +contact.getName());
+                AnContact contact = ContactTool.getAnContact(PersonalCenterActivity.this, 0);
+                Log.d("test", "contact" + contact.getName());
                 if (weatherOn) {
-                    WidgetTool.saveWidText(PersonalCenterActivity.this, "紧急联系人电话"+"\n"+editContact.getText().toString());
+                    WidgetTool.saveWidText(PersonalCenterActivity.this, "紧急联系人电话" + "\n" + editContact.getText().toString());
                     WidgetShow.updatewidget(PersonalCenterActivity.this);
-                    wid.showTextOnWallPaper(PersonalCenterActivity.this,  "紧急联系人电话", editContact.getText().toString());
-                    SharedPreferences.Editor editor=getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
+                    wid.showTextOnWallPaper(PersonalCenterActivity.this, "紧急联系人电话", editContact.getText().toString());
+                    SharedPreferences.Editor editor = getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
                     editor.putBoolean("contactButton", true);
                     editor.commit();
-                }else {
+                } else {
                     WidgetTool.saveWidText(PersonalCenterActivity.this, "老友网");
                     WidgetShow.updatewidget(PersonalCenterActivity.this);
                     wid.hideTextOnWallPaper(PersonalCenterActivity.this);
-                    SharedPreferences.Editor editor=getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
+                    SharedPreferences.Editor editor = getSharedPreferences("JudgeTwoToggle", MODE_PRIVATE).edit();
                     editor.putBoolean("contactButton", false);
                     editor.commit();
                 }
@@ -152,8 +150,8 @@ public class PersonalCenterActivity extends TActivity {
                     String contact_in = temp.getString("emer_contact");
                     String address_in = temp.getString("address");
                     //获得半径
-                    SharedPreferences pref=getSharedPreferences("Radius", MODE_PRIVATE);
-                    radius= pref.getString("radius", "100000000");
+                    SharedPreferences pref = getSharedPreferences("Radius", MODE_PRIVATE);
+                    radius = pref.getString("radius", "100000000");
                     if (!(name_in.equals("null"))) {
                         editName.setText(name_in);
                     }
@@ -174,7 +172,7 @@ public class PersonalCenterActivity extends TActivity {
                         );
                     }
                     if (!radius.equals("100000000")) {
-                       editradius.setText(radius);
+                        editradius.setText(radius);
                     }
 
 
@@ -194,22 +192,21 @@ public class PersonalCenterActivity extends TActivity {
     @Override
     public void showContextMenu() {
         showToast("按下按钮 ");
-        name=editName.getText().toString();
-        sex= editSex.getText().toString();
-        age= editAge.getText().toString();
-        ill=editIll.getText().toString();
-        location=editLocation.getText().toString();
-        contact=editContact.getText().toString();
-        radius=editradius.getText().toString();
-        try{
+        name = editName.getText().toString();
+        sex = editSex.getText().toString();
+        age = editAge.getText().toString();
+        ill = editIll.getText().toString();
+        location = editLocation.getText().toString();
+        contact = editContact.getText().toString();
+        radius = editradius.getText().toString();
+        try {
             Double.parseDouble(radius);
-            SharedPreferences.Editor editor=getSharedPreferences("Radius", MODE_PRIVATE).edit();
+            SharedPreferences.Editor editor = getSharedPreferences("Radius", MODE_PRIVATE).edit();
             editor.putString("radius", radius);
             editor.commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             showToast("请输入正确半径");
         }
-
 
 
         Connect.POST(this, ServerURL.Set_Personal_Message, new ConnectListener() {
@@ -217,19 +214,19 @@ public class PersonalCenterActivity extends TActivity {
 
             @Override
             public ConnectList setParam(ConnectList list) {
-                list.put("name",name);
-                list.put("age",age);
-                list.put("sex",sex);
-                list.put("common_ill",ill);
-                list.put("emer_contact",contact);
-                list.put("address",location);
+                list.put("name", name);
+                list.put("age", age);
+                list.put("sex", sex);
+                list.put("common_ill", ill);
+                list.put("emer_contact", contact);
+                list.put("address", location);
                 return list;
             }
 
             @Override
             public void onResponse(String response) {
                 Log.d("TAG", response);
-                if (response.equals("0")){
+                if (response.equals("0")) {
                     finish();
                 }
             }
@@ -246,17 +243,17 @@ public class PersonalCenterActivity extends TActivity {
 
 
     private void init() {
-        editName=(EditText)findViewById(R.id.edit_name);
-        editSex=(EditText)findViewById(R.id.edit_sex);
-        editAge= (EditText) findViewById(R.id.edit_age);
-        editIll= (EditText) findViewById(R.id.edit_ill);
-        editLocation= (EditText) findViewById(R.id.edit_location);
-        editContact=(EditText)findViewById(R.id.edit_contact);
-        editradius= (EditText) findViewById(R.id.edit_radius);
-        locationButton=(ToggleButton)findViewById(R.id.location_button);
-        contactButton= (ToggleButton) findViewById(R.id.contact_button);
-        centerBtn= (Button) findViewById(R.id.center_button);
-        outBtn= (Button) findViewById(R.id.out_button);
+        editName = (EditText) findViewById(R.id.edit_name);
+        editSex = (EditText) findViewById(R.id.edit_sex);
+        editAge = (EditText) findViewById(R.id.edit_age);
+        editIll = (EditText) findViewById(R.id.edit_ill);
+        editLocation = (EditText) findViewById(R.id.edit_location);
+        editContact = (EditText) findViewById(R.id.edit_contact);
+        editradius = (EditText) findViewById(R.id.edit_radius);
+        locationButton = (ToggleButton) findViewById(R.id.location_button);
+        contactButton = (ToggleButton) findViewById(R.id.contact_button);
+        centerBtn = (Button) findViewById(R.id.center_button);
+        outBtn = (Button) findViewById(R.id.out_button);
         wid = new WidgetImage();
         centerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -268,16 +265,17 @@ public class PersonalCenterActivity extends TActivity {
         outBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setResult(MainTabIndex.LOG_OUT, getIntent());
+                finish();
             }
         });
     }
 
-    private void initLocation(){
+    private void initLocation() {
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，
-        int span=1000;
+        int span = 1000;
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true);//可选，默认false,设置是否使用gps
@@ -286,7 +284,7 @@ public class PersonalCenterActivity extends TActivity {
         option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
         option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
         option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-        mLocationClient.setLocOption(option) ;
+        mLocationClient.setLocOption(option);
     }
 
 }
